34a25cff9c8f9a5b942c3d27ff684a31f7ed88f3
noobie-devil <73820820+noobie-devil@users.noreply.github.com>
2023-10-16 11:22:29 +0000

    Update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+import { Server } from 'socket.io';
 import {app, socketServer} from './src/app.js'
 import config from './src/utils/global.config.js'
+import { createServer } from "http";
 
-socketServer.on("connection", (socket) => {
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+io.on("connection", (socket) => {
   console.log("Connect socket")
 })
-const serverApp = app.listen(config.SERVER.PORT, () => {
+// socketServer.on("connection", (socket) => {
+//   console.log("Connect socket")
+// })
+// const serverApp = app.listen(config.SERVER.PORT, () => {
+//   console.log(`Example app listening on port ${config.SERVER.PORT}`)
+// })
+httpServer.listen(config.SERVER.PORT, () => {
   console.log(`Example app listening on port ${config.SERVER.PORT}`)
 })
 
-socketServer.attach(serverApp)
+// socketServer.attach(serverApp)
